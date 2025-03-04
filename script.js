7742e69da070e425b2c1d91eaa40e0e57b13d7cc
Akhmadjonovich <axrorbeknematov07@gmail.com>
2025-03-04 21:49:53 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,76 @@
+// task 1
+
+/*let pass1 = prompt(`Parol kiriting`);
+let pass2 = prompt(`Parolni qayta kiriting`);
+alert(`Parol: ${pass1 === pass2}`);
+
+//task 2
+
+/*let number = prompt(`Biror son kiriting `)
+number = Number(number)
+console.log(`Javob: ${number % 2 === 0}`)*/
+
+// task 3
+
+/*let num = prompt(`Raqam kiriting (kvadrat va kubga oshirish uchun):`);
+num = Number(num);
+console.log(`Kvadrat: ${num ** 2}`);
+console.log(`Kub: ${num ** 3}`);*/
+
+// task 4
+/*let tomon1 = +prompt(`To'g'ri to'rtburchakning birinchi tomonini kiriting: `)
+tomon1 = Number(tomon1)
+
+let tomon2 = +prompt(`To'g'ri to'rtburchakning ikkiinchi tomonini kiriting: `)
+tomon2 = Number(tomon2)
+
+console.log(`Yuzi: ${tomon1 * tomon2}`)
+console.log(`Perimetri: ${(tomon1 + tomon2)*2}`)*/
+
+//task 5
+
+/*let num1 = +prompt(`Birinchi sonni kiriting`);
+let num2 = +prompt(`Ikkinchi sonni kiriting`);
+let num3 = +prompt(`Uchinchi sonni kiriting`);
+
+let maxson = Math.max(num1, num2, num3);
+let minson = Math.min(num1, num2, num3);
+
+console.log(`Eng katta son : ${maxson}`);
+console.log(`Eng kichik son : ${minson}`);*/
+
+//task 6
+
+/*let num1 = +prompt(`Birinch sonni kiriting:`)
+let num2 = +prompt(`Ikkinch sonni kiriting:`)
+
+num1 = Number(num1)
+num2 = Number(num2)
+
+console.log(`Birinchi sonning kubini ikkinchisini kvadratiga nisbati: ${(num1 ** 3)/(num2 ** 2)}`)*/
+
+//task 7
+
+/*let dollor = +prompt(`Son kiriting dollorda:`)
+dollor = Number(dollor)
+
+console.log(`So'mda: ${ dollor*12897}`)*/
+
+// task 8
+
+/*let num1 = +prompt("1-raqamni kiriting:");
+let num2 = +prompt("2-raqamni kiriting:");
+
+let percent = (num2 / num1) * 100;
+
+console.log(`2-raqam 1-raqamning ${percent}% ga teng`);*/
+
+//task 9 
+
+/*let minut = +prompt(`Son kiriting minutda:`)
+minut = Number(minut)
+
+console.log(`${minut} minut ${minut*60} secundga teng`)*/
+
+// task 10 
+/* Bajarildi*/
